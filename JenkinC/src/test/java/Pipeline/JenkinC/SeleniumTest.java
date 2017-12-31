9e6bd31dcc2c916e96af940d7ff03d9f32f34e40
souvikchandra1984 <souvik.chandra@gmail.com>
2017-12-31 17:40:06 +0530

    First Commit

---
+++ JenkinC/src/test/java/Pipeline/JenkinC/SeleniumTest.java
@@ -0,0 +1,19 @@
+package Pipeline.JenkinC;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+@Test
+public class SeleniumTest {
+  
+	public void testfun1()
+	{
+		System.setProperty("webdriver.gecko.driver", "D://Souvik//Selenium//Selenium Jar//geckodriver-v0.11.1-win64//geckodriver.exe");
+		WebDriver d = new FirefoxDriver();
+		d.get("http://www.google.com");
+		System.out.println(d.getPageSource());
+		System.out.println(d.getWindowHandle());
+		//d.quit();
+	}
+	
+}
