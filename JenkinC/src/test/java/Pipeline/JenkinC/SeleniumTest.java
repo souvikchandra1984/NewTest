40a9cdbc229cebcf01c4d45967d6dbd7bbafaf79
souvikchandra1984 <souvik.chandra@gmail.com>
2018-01-01 19:53:43 +0530

    Check Integrity.

--- JenkinC/src/test/java/Pipeline/JenkinC/SeleniumTest.java
+++ JenkinC/src/test/java/Pipeline/JenkinC/SeleniumTest.java
@@ -10,7 +10,7 @@ public class SeleniumTest {
 	public void testfun1()
 	{
 		System.out.println("SeleniumTest");
-		System.setProperty("webdriver.gecko.driver", "D://Souvik//Selenium//geckodriver-v0.19.1-win64//geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", "D://Souvik//Selenium//geckodriver-v0.19.1-win64//geckodriver1.exe");
 		WebDriver d = new FirefoxDriver();
 		d.get("http://www.google.com");
 		System.out.println(d.getPageSource());
